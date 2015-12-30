b16014940eb9fc840979c572524315dbf8b8b3e3
shimizukawa <shimizukawa@gmail.com>
2015-12-30 18:10:26 +0900

    remove old pot files

--- locale/update.sh
+++ locale/update.sh
@@ -1,6 +1,7 @@
 export SPHINXINTL_TRANSIFEX_PROJECT_NAME=sphinx-doc-1_2_0
 
 cd `dirname $0`
+rm -R pot
 sphinx-build -b gettext ../sphinx/doc pot
 sphinx-intl update-txconfig-resources -p pot -d .
 rm -R es ja pt_BR

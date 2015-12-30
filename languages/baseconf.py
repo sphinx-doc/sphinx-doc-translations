d785bef0c195f4eaab18a882610b5ca3e38011d9
shimizukawa <shimizukawa@gmail.com>
2015-12-30 17:03:08 +0900

    fix: preserve original setup function in conf.py

--- languages/baseconf.py
+++ languages/baseconf.py
@@ -4,7 +4,11 @@ execfile(os.path.join(BASEDIR, 'sphinx/doc/conf.py'))
 
 locale_dirs = [os.path.join(BASEDIR, 'locale/')]
 
+setup_original = setup
+
 def setup(app):
     app.srcdir = os.path.join(BASEDIR, 'sphinx/doc/')
     app.confdir = app.srcdir
+
+    setup_original(app)
 

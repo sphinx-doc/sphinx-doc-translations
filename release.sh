49e771e87e83219dd1ef437f9fa18ba0ed1aaeb3
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-06-12 22:38:21 +0900

    Add release.sh

---
+++ release.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$1" ]; then
+    echo "$0 [version]"
+    exit 1
+fi
+
+RELEASE=$1
+BRANCH=`echo $RELEASE | cut -d . -f 1,2`
+
+git submodule init
+git submodule update
+(cd sphinx; git fetch origin)
+
+# update x.y branch
+git co $BRANCH
+cd sphinx
+git co $RELEASE
+cd ../locale
+pip install -r requirements.txt
+sphinx-intl create-transifexrc
+sh update.sh
+cd ..
+git add locale sphinx
+git commit -am "use sphinx-$RELEASE"
+
+# update stable branch
+git co stable
+cd sphinx
+git co $RELEASE
+cd ../locale
+pip install -r requirements.txt
+sphinx-intl create-transifexrc
+sh update.sh
+cd ..
+git add locale sphinx
+git commit -am "use sphinx-$RELEASE"
+
+# push changes
+git co master
+git submodule update
+git push origin stable $BRANCH

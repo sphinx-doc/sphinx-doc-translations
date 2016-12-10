8fa719b80d7da23ba0fc1cf6a64f47d407c44f6d
shimizukawa <shimizukawa@gmail.com>
2016-12-10 16:30:10 +0900

    use sphinx-1.5

--- locale/update.sh
+++ locale/update.sh
@@ -1,10 +1,14 @@
+#!/bin/sh
+
+set -ex
+
 export SPHINXINTL_TRANSIFEX_PROJECT_NAME=sphinx-doc-1_5
 
 cd `dirname $0`
 rm -R pot
 sphinx-build -b gettext ../sphinx/doc pot
 sphinx-intl update-txconfig-resources -p pot -d .
-rm -R es ja pt_BR
+rm -Rf es ja pt_BR
 tx pull -l es,ja,pt_BR
 git checkout .tx/config
 

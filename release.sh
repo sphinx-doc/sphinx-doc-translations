0e95ebfb4750ac50c518cd24423cda9593d0e73e
shimizukawa <shimizukawa@gmail.com>
2018-02-20 00:36:58 +0900

    use sphinx 1.6 tag

--- release.sh
+++ release.sh
@@ -2,16 +2,16 @@
 
 set -ex
 
-RELEASE=1_7
+RELEASE=1_6
 
 # setup environment
 SPHINXINTL_TRANSIFEX_USERNAME=sphinxjp
 SPHINXINTL_TRANSIFEX_PROJECT_NAME=sphinx-doc-${RELEASE}
 find sphinx -name "*.pyc" -exec rm {} \;
-git checkout 1.7
+git checkout 1.6
 git submodule init
 git submodule update
-(cd sphinx; git fetch origin; git checkout -b 1.7 origin/1.7)
+(cd sphinx; git fetch origin; git checkout 1.6)
 pip install -r requirements.txt
 
 
@@ -23,6 +23,6 @@ git add locale sphinx
 git commit -m "[skip ci] update po(t) files"
 
 # push changes
-git checkout 1.7
+git checkout 1.6
 git submodule update
-git push origin 1.7
+git push origin 1.6

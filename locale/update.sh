86520c24d00b887b3489520b33867358628fceb1
Tetsuo Koyama <tkoyama010@gmail.com>
2020-04-12 14:17:30 +0900

    :rocket: update.sh

--- locale/update.sh
+++ locale/update.sh
@@ -10,12 +10,12 @@ set -ex
 
 # pull po files from transifex
 cd `dirname $0`
-sphinx-intl create-transifexrc
+/home/runner/.local/bin/sphinx-intl create-transifexrc
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-sphinx-build -T -b gettext ../sphinx/doc pot
-sphinx-intl update-txconfig-resources -p pot -d .
+/home/runner/.local/bin/sphinx-build -T -b gettext ../sphinx/doc pot
+/home/runner/.local/bin/sphinx-intl update-txconfig-resources -p pot -d .
 cat .tx/config
-tx push -s --skip
+/home/runner/.local/bin/tx push -s --skip
 rm -R -f ar ca_ES zh_CN fr de it_IT ja ko pl_PL pt_BR ru sr sr_RS es
 tx pull -l ar,ca_ES,zh_CN,fr,de,it_IT,ja,ko,pl_PL,pt_BR,ru,sr,sr_RS,es
 git checkout .tx/config

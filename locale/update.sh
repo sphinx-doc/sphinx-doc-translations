3d725adfc594398b15a87e84cf5c39dd58728229
Tetsuo Koyama <tkoyama010@gmail.com>
2023-02-07 09:09:21 +0900

    Bump sphinx-intl[transifex] from 2.0.1 to 2.1.0

--- locale/update.sh
+++ locale/update.sh
@@ -10,10 +10,10 @@ set -ex
 
 # pull po files from transifex
 cd `dirname $0`
-sphinx-intl create-transifexrc
+sphinx-intl create-transifexrc --transifex-token=$TX_TOKEN
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
 sphinx-build -T -b gettext ../sphinx/doc pot
-sphinx-intl update-txconfig-resources -p pot -d .
+sphinx-intl update-txconfig-resources -p pot -d . --transifex-organization-name="sphinx-doc"
 cat .tx/config
 tx push -s --skip
 rm -R -f ar ca_ES zh_CN fr de it_IT ja ko pl_PL pt_BR ru sr sr_RS es

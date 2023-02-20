f9a5e8b2ed660e5cd43ffc29167de9d0f6ffb0f0
shimizukawa <shimizukawa@gmail.com>
2023-02-20 20:14:23 +0000

    [WIP] trying fix of all deletion at 6a428662c1ad75aa99ad9c35557de748b487d4e7

--- locale/update.sh
+++ locale/update.sh
@@ -6,13 +6,10 @@ set -ex
 # required environment variables
 # SPHINXINTL_TRANSIFEX_ORGANIZATION_NAME=sphinx-doc
 # SPHINXINTL_TRANSIFEX_PROJECT_NAME=sphinx-doc
-# SPHINXINTL_TRANSIFEX_USERNAME=sphinxjp
-# SPHINXINTL_TRANSIFEX_TOKEN=...
-
+# TX_TOKEN=...
 
 # pull po files from transifex
 cd `dirname $0`
-sphinx-intl create-transifexrc
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
 sphinx-build -T -b gettext ../sphinx/doc pot
 sphinx-intl update-txconfig-resources -p pot -d .

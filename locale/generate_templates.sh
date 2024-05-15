6676bef9c1abaea7c57e1a937e25a658ddd5361c
Rafael Fontenelle <rffontenelle@gmail.com>
2024-05-15 16:10:57 -0300

    update.sh: move POT and config into generate_template.sh
    This step is required for later deploying a script that
    removes obsolete resources in Transifex

---
+++ locale/generate_templates.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Update POT files and Transifex configuration file
+
+set -ex
+
+# required environment variables
+# SPHINXINTL_TRANSIFEX_ORGANIZATION_NAME=sphinx-doc
+# SPHINXINTL_TRANSIFEX_PROJECT_NAME=sphinx-doc
+# TX_TOKEN=...
+
+cd `dirname $0`
+#rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
+sphinx-build -T -b gettext ../sphinx/doc pot
+sphinx-intl update-txconfig-resources -p pot -d .
--- locale/update.sh
+++ locale/update.sh
@@ -7,17 +7,8 @@ LANGS='ar ca_ES de es fr it_IT ja ko pl_PL pt_BR ru sr zh_CN'
 
 LANGS_PULL=$(echo $LANGS | sed 's| |,|g')
 
-# required environment variables
-# SPHINXINTL_TRANSIFEX_ORGANIZATION_NAME=sphinx-doc
-# SPHINXINTL_TRANSIFEX_PROJECT_NAME=sphinx-doc
-# TX_TOKEN=...
-
 # pull po files from transifex
 cd `dirname $0`
-#rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-sphinx-build -T -b gettext ../sphinx/doc pot
-sphinx-intl update-txconfig-resources -p pot -d .
-cat .tx/config
 
 # Skip Transifex commands when running pull requests event on GitHub Actions
 if [[ "$GITHUB_EVENT_NAME" != 'pull_request' ]]; then

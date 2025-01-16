8bbe0ad154a5939b2c9e9749a6055bd6c8e63de0
Takayuki SHIMIZUKAWA <shimizukawa@gmail.com>
2025-01-16 06:42:40 +0000

    upload environment.pickle as artifacts for investigation

--- locales/generate_templates.sh
+++ locales/generate_templates.sh
@@ -10,5 +10,5 @@ set -ex
 
 cd `dirname $0`
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-sphinx-build -E -T -j auto -b gettext ../sphinx/doc pot
+sphinx-build -T -j auto -b gettext ../sphinx/doc pot
 sphinx-intl update-txconfig-resources -p pot -d .

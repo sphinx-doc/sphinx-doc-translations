02c1c70e8d8f415c4078fc6c8783877e04985818
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2025-11-29 08:40:15 -0300

    Install dependency group along with requirements.txt (#71)
    * Install dependency group along with requirements.txt
    
    * Simplify submodule update command in workflow
    
    * Install latest Sphinx
    
    * Improve requirements.txt note
    
    * Commit suggestion from review
    
    Co-authored-by: Adam Turner <9087854+AA-Turner@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Adam Turner <9087854+AA-Turner@users.noreply.github.com>

--- .devcontainer/on_create_command.sh
+++ .devcontainer/on_create_command.sh
@@ -5,6 +5,8 @@ set -ex
 
 sudo apt-get update && sudo apt-get install -y graphviz
 pip install -U pip setuptools
+pip install -e sphinx
+pip install --group sphinx/pyproject.toml:docs
 pip install -r requirements.txt
 
 # Install Transifex CLI tool into /usr/local/bin
